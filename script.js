c5d4a022d70962835c476609dd141587f059c330
Artemtar <artemtarasov89@gmail.com>
2020-02-29 20:46:41 -0500

    test case added; server side script update; modules update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const socket = io('http://localhost:3000')
 
-io.on('connection', socket => {
-    socket.emit('chat-message', 'hello wotld')
+socket.on('chat-message', data => {
+    console.log(data);
 })
