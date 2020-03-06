c95ee2c27fec35bec0fa090cf3b46c6ef108c3d7
Artemtar <artemtarasov89@gmail.com>
2020-03-05 23:45:31 -0500

    cleanup

--- lamdas/test.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const AWS = require('aws-sdk');
-AWS.config.update({region:"us-west-1"});
-exports.handler = function (event, context, callback) {
-    console.log(JSON.stringify(`Event: event`))
-    const ddb = AWS.Dy
-}
