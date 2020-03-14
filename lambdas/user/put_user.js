6eee281589614463713edd47e66c09433e977fc0
Artemtar <artemtarasov89@gmail.com>
2020-03-13 23:28:30 -0400

    fixed add new user endpoint

--- lambdas/user/put_user.js
+++ lambdas/user/put_user.js
@@ -2,25 +2,36 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 exports.handler = async (event, context) => {
-    const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+
     let responseBody = '';
     let statusCode = 0;
-    const { userId, name, conversationId } = JSON.parse(event['body'])
+    console.log('HELLO ');
+    console.log(event['body']);
+    console.log(event);
+    
+    const { userId, nickName, pwd } = JSON.parse(event["body"]);
+    
+    console.log(userId);
+
     const params = {
         TableName: 'User',
         Item: {
-            userId: userId,
-            userName: name,
-            conversation: conversationId
+            'userId': userId,
+            'nickName': nickName,
+            'pwd': pwd
         }
+
     };
     try {
         const data = await documentClient.put(params).promise();
         responseBody = JSON.stringify(data);
         statusCode = 201;
-    } catch (err) {
+        responseBody += `Succesfuly add new user ${userId}`;
 
+    } catch (err) {
         responseBody = `Unable to put user: ${err}`;
+        responseBody += event["body"] + ' ' + nickName;
         statusCode = 403;
     }
     const response = {
@@ -32,4 +43,4 @@ exports.handler = async (event, context) => {
         body: responseBody
     };
     return response;
-}
+};
